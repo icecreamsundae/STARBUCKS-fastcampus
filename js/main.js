75bc802eb4d88c079812f8a15b021bdf7d768f58
icecreamsundae <eunjeongkeem@gmail.com>
2022-07-02 21:30:21 +0900

    starbucks landing page

--- js/main.js
+++ js/main.js
@@ -77,12 +77,12 @@ fadeEls.forEach(function (fadeEl, index) {
 
 /*SWIPER*/
 /*new Swiper(선택자, 옵션) : 생성자*/
-new Swiper('.notice-line .swiper-container', {
+new Swiper('.notice-line .swiper', {
   direction: 'vertical', // 수직 슬라이드
   autoplay: true, // 자동 재생 여부
   loop: true // 반복 재생 여부
 });
-new Swiper('.promotion .swiper-container', {
+new Swiper('.promotion .swiper', {
   // direction: 'horizontal', // 수평 슬라이드
   autoplay: { 
     delay: 5000 // 5초마다 슬라이드 바뀜
@@ -102,7 +102,7 @@ new Swiper('.promotion .swiper-container', {
     nextEl: '.promotion .swiper-next' // 다음 버튼 선택자
   }
 });
-new Swiper('.awards .swiper-container', {
+new Swiper('.awards .swiper', {
   // direction: 'horizontal', // 수평 슬라이드
   autoplay: true, // 자동 재생 여부
   loop: true, // 반복 재생 여부
@@ -153,3 +153,20 @@ function floatingObject(selector, delay, size) {
 floatingObject('.floating1', 1, 15); //selector, delay, size
 floatingObject('.floating2', 5, 15);
 floatingObject('.floating3', 1.5, 20);
+
+/* 요소가 화면에 보여짐 여부에 따른 요소 관리 */
+const spyEls = document.querySelectorAll('section.scroll-spy')
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({ // 감시할 장면(Scene)을 추가
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8 // 화면의 80% 지점에서 보여짐 여부 감시
+    })
+    .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+    .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당(필수!)
+})
+
+
+/*올해가 몇 년도인지 계산*/
+const thisYear = document.querySelector('.this-year')
+thisYear.textContent = new Date().getFullYear()
